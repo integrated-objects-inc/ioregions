16e72f8817d4d8cdd53c851844841531796192ec
rickcoker <rick.coker@iobjectsinc.com>
2015-10-01 05:45:16 -0500

    Initial commit

---
+++ ioregions/Modules/Common/DatabaseEnums.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ioregions.Modules.Common
+{
+    public class DatabaseEnums
+    {
+        public enum States
+        {
+            AA = 5,
+            AE = 6,
+            Alaska = 7,
+            Alabama = 8,
+            AP = 9,
+            Arkansas = 10,
+            American_Samoa = 11,
+            Arizona = 12,
+            California = 13,
+            Colorado = 14,
+            Connecticut = 15,
+            Washington_DC = 16,
+            Deleware = 17,
+            Florida = 18,
+            Federated_States_of_Micronesia = 19,
+            Georgia = 20,
+            Guam = 21,
+            Hawaii = 22,
+            Iowa = 23,
+            Idaho = 24,
+            Illinois = 25,
+            Indiana = 26,
+            Kansas = 27,
+            Kentucky = 28,
+            Louisiana = 29,
+            Massachuets = 30,
+            Maryland = 31,
+            Maine = 32,
+            Marshall_Islands = 33,
+            Michigan = 34,
+            Minnesota = 35,
+            Missouri = 36,
+            Northern_Mariana_Islands = 37,
+            Mississippi = 38,
+            Montana = 39,
+            North_Carolina = 40,
+            North_Dakota = 41,
+            Nebraska = 42,
+            New_Hampshire = 43,
+            New_Jersey = 44,
+            New_Mexico = 45,
+            Nevada = 46,
+            New_York = 47,
+            Ohio = 48,
+            Oklahoma = 49,
+            Oregon = 50,
+            Pennsylvania = 51,
+            Puerto_Rico = 52,
+            Republic_of_Palau = 53,
+            Rhode_Island = 54,
+            South_Carolina = 55,
+            South_Dakota = 56,
+            Tennessee = 57,
+            Texas = 58,
+            Utah = 59,
+            Virginia = 60,
+            Virgin_Islands = 61,
+            Vermont = 62,
+            Washington = 63,
+            Wisconsin = 64,
+            West_Virginia = 65,
+            Wyoming = 66
+        }
+    }
+}
